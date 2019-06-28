9959d11256cd37daef71ce0abecfb4892dfbe190
Marcos Lyrio <sampaiolyrio@hotmail.com>
2019-06-27 23:43:57 -0300

    aula 1.1

--- app/src/main/java/br/com/lyrio/minhaagenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/lyrio/minhaagenda/ListaAlunosActivity.java
@@ -3,11 +3,11 @@ package br.com.lyrio.minhaagenda;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
     }
 }
