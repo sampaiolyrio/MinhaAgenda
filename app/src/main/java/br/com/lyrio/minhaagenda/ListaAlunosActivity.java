c1bd0d840ccb48cded3b7e79ad67bab79036e540
Marcos Lyrio <sampaiolyrio@hotmail.com>
2019-06-28 09:18:10 -0300

    Aula 1.3

--- app/src/main/java/br/com/lyrio/minhaagenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/lyrio/minhaagenda/ListaAlunosActivity.java
@@ -2,6 +2,8 @@ package br.com.lyrio.minhaagenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Marcos", "Lídia", "Isabel", "Luciana"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
     }
 }
