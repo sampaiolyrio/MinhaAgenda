e41f0ad2d6e376830e7e33fa9d0eac77e3ca2727
Marcos Lyrio <sampaiolyrio@hotmail.com>
2019-06-28 20:24:51 -0300

    Aula 3.3 Signed-off-by: Marcos Lyrio <sampaiolyrio@hotmail.com>

--- app/src/main/java/br/com/lyrio/minhaagenda/FormularioActivity.java
+++ app/src/main/java/br/com/lyrio/minhaagenda/FormularioActivity.java
@@ -6,6 +6,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,15 +20,25 @@ public class FormularioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
-        botaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                Toast.makeText(FormularioActivity.this, "Dados Salvos!", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
+            Toast.makeText(FormularioActivity.this, "Dados Salvos!", Toast.LENGTH_SHORT).show();
+            finish();
+            break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
