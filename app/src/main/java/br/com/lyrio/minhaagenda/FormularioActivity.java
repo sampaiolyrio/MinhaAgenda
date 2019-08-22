4eeda7a50552eb615965782085b18ccc5447a93a
Marcos Lyrio <sampaiolyrio@hotmail.com>
2019-08-22 11:25:45 -0300

    Curso Android II Integração com apps e recursos do device 2. Usando a câmera Aula 2.3 Signed-off-by: Marcos Lyrio <sampaiolyrio@hotmail.com>

--- app/src/main/java/br/com/lyrio/minhaagenda/FormularioActivity.java
+++ app/src/main/java/br/com/lyrio/minhaagenda/FormularioActivity.java
@@ -1,8 +1,12 @@
 package br.com.lyrio.minhaagenda;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -10,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -20,7 +25,9 @@ import br.com.lyrio.minhaagenda.modelo.Pessoa;
 
 public class FormularioActivity extends AppCompatActivity {
 
+    public static final int CODIGO_CAMERA = 567;
     private FormularioHelper helper;
+    private String caminhoFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,21 +45,33 @@ public class FormularioActivity extends AppCompatActivity {
         }
 
 
-        Button botaoFoto = findViewById(R.id.formulario_botao_foto);
+        Button botaoFoto = (Button) findViewById(R.id.formulario_botao_foto);
         botaoFoto.setOnClickListener(new View.OnClickListener() {
-                                         @Override
-                                         public void onClick(View view) {
-                                             Intent intentCamera=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                                             String caminhoFoto = getExternalFilesDir(null)+"/"+ System.currentTimeMillis()+".jpg";
-                                             File arquivoFoto = new File(caminhoFoto);
-                                             intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivoFoto));
-                                             startActivity (intentCamera);
+            @Override
+            public void onClick(View v) {
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                caminhoFoto = getExternalFilesDir(null) + "/" + System.currentTimeMillis() + ".jpg";
+                File arquivoFoto = new File(caminhoFoto);
+                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(arquivoFoto));
+                startActivityForResult(intentCamera, CODIGO_CAMERA);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == Activity.RESULT_OK) {
+            if (requestCode == CODIGO_CAMERA) {
+                ImageView foto = (ImageView) findViewById(R.id.formulario_foto);
+                Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+                Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 300, 300, true);
+                foto.setImageBitmap(bitmapReduzido);
+                foto.setScaleType(ImageView.ScaleType.FIT_XY);
 
-                                         }
-                                     }
-        );
+            }
 
+        }
 
+        // abrir a foto que foi tirada
     }
 
     @Override
