3199fcaaaccb4997a73bedf088439d0ce992999b
Marcos Lyrio <sampaiolyrio@hotmail.com>
2019-08-22 10:00:59 -0300

    Curso Android II Integração com apps e recursos do device 2. Usando a câmera Aula 2.2 Signed-off-by: Marcos Lyrio <sampaiolyrio@hotmail.com>

--- app/src/main/java/br/com/lyrio/minhaagenda/FormularioActivity.java
+++ app/src/main/java/br/com/lyrio/minhaagenda/FormularioActivity.java
@@ -1,13 +1,18 @@
 package br.com.lyrio.minhaagenda;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.Serializable;
 
 import br.com.lyrio.minhaagenda.dao.PessoaDAO;
@@ -33,6 +38,19 @@ public class FormularioActivity extends AppCompatActivity {
         }
 
 
+        Button botaoFoto = findViewById(R.id.formulario_botao_foto);
+        botaoFoto.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View view) {
+                                             Intent intentCamera=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                             String caminhoFoto = getExternalFilesDir(null)+"/"+ System.currentTimeMillis()+".jpg";
+                                             File arquivoFoto = new File(caminhoFoto);
+                                             intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivoFoto));
+                                             startActivity (intentCamera);
+
+                                         }
+                                     }
+        );
 
 
     }
