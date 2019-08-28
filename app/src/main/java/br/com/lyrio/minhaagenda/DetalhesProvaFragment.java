789687c23c635178437b5eaafe07345d269635f0
Marcos Lyrio <sampaiolyrio@hotmail.com>
2019-08-28 20:32:45 -0300

    Curso Android III Aprimore sua app com Fragments, Google Maps e GPS 2. Reproveitando telas com Fragments Aula 2.5 Signed-off-by: Marcos Lyrio <sampaiolyrio@hotmail.com>

--- app/src/main/java/br/com/lyrio/minhaagenda/DetalhesProvaFragment.java
+++ app/src/main/java/br/com/lyrio/minhaagenda/DetalhesProvaFragment.java
@@ -6,18 +6,48 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.List;
+
+import br.com.lyrio.minhaagenda.modelo.Prova;
 
 
 public class DetalhesProvaFragment extends Fragment {
 
+
+    private TextView campoMateria;
+    private TextView campoData;
+    private ListView listaTopicos;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_detalhes_prova, container, false);
 
+        campoMateria = (TextView) view.findViewById(R.id.detalhes_prova_materia);
+        campoData = view.findViewById(R.id.detalhes_prova_data);
+        listaTopicos = view.findViewById(R.id.detalhes_prova_topicos);
+
+        Bundle parametros = getArguments();
+        if(parametros != null){
+            Prova prova = (Prova) parametros.getSerializable("prova");
+            populaCamposCom(prova);
+        }
+
+
         return view;
+    }
+
+    public void populaCamposCom(Prova prova){
+
+        campoMateria.setText(prova.getMateria());
+        campoData.setText(prova.getData());
+        ArrayAdapter<String> adapterTopicos =
+                new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1, prova.getTopicos());
+        listaTopicos.setAdapter(adapterTopicos);
     }
 
 }
--- app/src/main/java/br/com/lyrio/minhaagenda/ProvasActivity.java
+++ app/src/main/java/br/com/lyrio/minhaagenda/ProvasActivity.java
@@ -1,6 +1,7 @@
 package br.com.lyrio.minhaagenda;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -38,5 +39,27 @@ public class ProvasActivity extends AppCompatActivity {
 
     private boolean estaNoModoPaisagem() {
         return getResources().getBoolean(R.bool.modoPaisagem);
+    }
+
+    public void selecionaProva(Prova prova) {
+        FragmentManager manager = getSupportFragmentManager();
+        if (!estaNoModoPaisagem()) {
+            FragmentTransaction tx = manager.beginTransaction();
+
+            DetalhesProvaFragment detalhesFragment = new DetalhesProvaFragment();
+            Bundle parametros = new Bundle();
+            parametros.putSerializable("prova", prova);
+            detalhesFragment.setArguments(parametros);
+
+            tx.replace(R.id.frame_principal, detalhesFragment);
+            tx.commit();
+        } else{
+            DetalhesProvaFragment  detalhesFragment =
+                    (DetalhesProvaFragment) manager.findFragmentById(R.id.frame_secundario);
+            detalhesFragment.populaCamposCom(prova);
+
+
+
+        }
     }
 }
